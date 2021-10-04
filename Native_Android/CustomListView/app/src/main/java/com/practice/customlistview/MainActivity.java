61442587d371772a0d7f2bb5c2fad78813dc2897
Harshit Verma <harshitverma14366@gmail.com>
2021-10-04 16:01:22 +0530

    new

--- Native_Android/CustomListView/app/src/main/java/com/practice/customlistview/MainActivity.java
+++ Native_Android/CustomListView/app/src/main/java/com/practice/customlistview/MainActivity.java
@@ -5,10 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int[] IMAGES = {R.drawable.friend1,
+            R.drawable.friend2,
+            R.drawable.friend3,
+            R.drawable.friend4,
+            R.drawable.friend5,
+            R.drawable.friend6,
+            R.drawable.friend7,
+            R.drawable.friend8,
+            R.drawable.friend9,
+            R.drawable.friend10 };
 
+    String[] NAMES = {"Lesa Bates",
+            "Mabel Young",
+            "Thomas Rice",
+            "Alyssa James",
+            "Tracy Hughes",
+            "Jean Coleman",
+            "Tyler Young",
+            "Felix Washington",
+            "Andre Green",
+            "Brandie Jennings"};
+
+
+    String[] PHONES = {"588-930-2178",
+            "782-760-6801",
+            "800-500-7103",
+            "810-438-1930",
+            "651-305-9242",
+            "563-355-7671",
+            "648-179-5711",
+            "424-993-1478",
+            "244-336-5495",
+            "159-193-7791"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- Native_Android/CustomListView/app/src/main/java/com/practice/customlistview/MyAdapterClass.java
+++ Native_Android/CustomListView/app/src/main/java/com/practice/customlistview/MyAdapterClass.java
@@ -1,4 +1,44 @@
 package com.practice.customlistview;
 
-public class MyAdapterClass {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+public class MyAdapterClass extends ArrayAdapter<String> {
+    private final Activity context;
+    private final int[] image;
+    private final String[] Username;
+    private final String[] phoneNumber;
+
+    //Alt+Shift+insert ------> create only default Constructor
+    public MyAdapterClass(@NonNull Context context, int resource, Activity context1, int[] image, String[] username, String[] phoneNumber) {
+        super(context, resource);
+        this.context = context1;
+        this.image = image;
+        Username = username;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //We need to create a getView(View view) method
+
+    public View getView(int position, View view, ViewGroup parent)
+    {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.one_item_layout,null,true);
+
+        TextView UserNum=rowView.findViewById(R.id.textView1);
+        TextView MobileNum=rowView.findViewById(R.id.textView2);
+        ImageView imgView=rowView.findViewById(R.id.imageView);
+        UserNum.setText(Username[position]);
+        MobileNum.setText(phoneNumber[position]);
+        imgView.setImageResource(image[position]);
+        return rowView;
+    }
 }
