baad207a2590c3c6a7b9f94edc300f0c519b9914
Harshit Verma <harshitverma14366@gmail.com>
2021-10-05 19:51:58 +0530

    new

--- Native_Android/Chat App/TapMeForChitChat/app/src/main/java/com/practice/tapmeforchitchat/MainChatActivity.java
+++ Native_Android/Chat App/TapMeForChitChat/app/src/main/java/com/practice/tapmeforchitchat/MainChatActivity.java
@@ -1,13 +1,47 @@
 package com.practice.tapmeforchitchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainChatActivity extends AppCompatActivity {
+
+    private String myUserName;
+    private ListView myChatListView;
+    private EditText myChatText;
+    private ImageButton mySendChatButton;
+    private DatabaseReference myDatabaseRef;
+    private ChatListAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat);
+        setUpDisplayName();
+        myDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        //Get UI elements refs
+        myChatListView=findViewById(R.id.chat_list_view);
+        myChatText=findViewById(R.id.messageInput);
+        mySendChatButton=findViewById(R.id.sendButton);
     }
+    // Set UserName for user
+    private void setUpDisplayName()
+    {
+        SharedPreferences prefs=getSharedPreferences(RegisterActivity.CHAT_PREF,MODE_PRIVATE);
+        myUserName=prefs.getString(RegisterActivity.DISPLAY_NAME,null);
+        if(myUserName==null)
+        {
+            myUserName="user";
+        }
+    }
+
+
 }
